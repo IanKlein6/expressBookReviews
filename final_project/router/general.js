29783fc2c95126b59484f04e997bcf41b3f1eae8
Ian Klein <ianklein66@gmail.com>
2023-07-16 15:12:13 +0200

    promises for title, author, isbn, books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,6 +26,16 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get('/books', function (req, res) {
   res.send(JSON.stringify({ books }, null, 4));
+  
+});
+
+//Get books via promises
+public_users.get('/books',function (req, res) {
+  const get_books = new Promise((resolve, reject) => {
+      resolve(res.send(JSON.stringify({books}, null, 4)));
+    });
+
+    get_books.then(() => console.log("Promise resolved"));
 });
 
 // Get book details based on ISBN
@@ -40,6 +50,29 @@ public_users.get('/isbn/:isbn', function (req, res) {
   }
 });
 
+// Get book details based on ISBN using Promises
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+
+  const getBookByISBN = new Promise((resolve, reject) => {
+    if (books.hasOwnProperty(isbn)) {
+      const book = books[isbn];
+      resolve(book);
+    } else {
+      reject({ message: "Book not found" });
+    }
+  });
+
+  getBookByISBN
+    .then((book) => {
+      res.json(book);
+    })
+    .catch((error) => {
+      res.status(404).json(error);
+    });
+});
+
+
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
@@ -52,6 +85,35 @@ public_users.get('/author/:author', function (req, res) {
   }
 });
 
+//Get book details based on author via promises
+public_users.get('/books/author/:author',function (req, res) {
+
+  const get_books_author = new Promise((resolve, reject) => {
+
+  let booksbyauthor = [];
+  let isbns = Object.keys(books);
+  isbns.forEach((isbn) => {
+    if(books[isbn]["author"] === req.params.author) {
+      booksbyauthor.push({"isbn":isbn,
+                          "title":books[isbn]["title"],
+                          "reviews":books[isbn]["reviews"]});
+    resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+    }
+
+
+  });
+  reject(res.send("The mentioned author does not exist "))
+      
+  });
+
+  get_books_author.then(function(){
+          console.log("Promise is resolved");
+ }).catch(function () { 
+              console.log('The mentioned author does not exist');
+});
+
+});
+
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
@@ -62,6 +124,36 @@ public_users.get('/title/:title', function (req, res) {
   } else {
     return res.status(404).json({ message: "No books found with the title" });
   }
+});
+
+
+//Get book details based on titles via promises
+public_users.get('/books/title/:title',function (req, res) {
+
+  const get_books_title = new Promise((resolve, reject) => {
+
+  let booksbytitle = [];
+  let isbns = Object.keys(books);
+  isbns.forEach((isbn) => {
+    if(books[isbn]["title"] === req.params.title) {
+      booksbytitle.push({"isbn":isbn,
+                          "title":books[isbn]["title"],
+                          "reviews":books[isbn]["reviews"]});
+    resolve(res.send(JSON.stringify({booksbytitle}, null, 4)));
+    }
+
+
+  });
+  reject(res.send("The mentioned title does not exist "))
+      
+  });
+
+  get_books_title.then(function(){
+          console.log("Promise is resolved");
+ }).catch(function () { 
+              console.log('The mentioned title does not exist');
+});
+
 });
 
 // Get book review
